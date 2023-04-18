142948f65959edc12f90611affefa65f7f7c604a
ALittleDaisy <1152594956@qq.com>
2023-04-18 17:47:04 +0800

    添加自定义键

--- app/src/main/java/com/chestnut/warhero/MainActivity.java
+++ app/src/main/java/com/chestnut/warhero/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
         crashButton.setText("Test Crash");
         crashButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+                crashlytics.setCustomKey("my_string_key", "foo" /* string value */);
+
+                crashlytics.setCustomKey("my_bool_key", true /* boolean value */);
+
+                crashlytics.setCustomKey("my_double_key", 1.0 /* double value */);
+
+                crashlytics.setCustomKey("my_float_key", 1.0f /* float value */);
+
+                crashlytics.setCustomKey("my_int_key", 1 /* int value */);
+
                 throw new RuntimeException("Test Crash"); // Force a crash
             }
         });
